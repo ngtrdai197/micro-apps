e8c3f65da783b99b2330863bb6a1e7b495f06ef2
Dai Nguyen <ngtrdai290197@gmail.com>
2024-02-26 01:24:52 +0700

    remove file unused

--- account-service/target/debug/deps/account_service-abce2ba0f2188f03.d
+++
@@ -1,5 +0,0 @@
-/Users/dainguyen.iammm/Documents/Code/Personal/Rust/micro-apps/account-service/target/debug/deps/libaccount_service-abce2ba0f2188f03.rmeta: src/main.rs
-
-/Users/dainguyen.iammm/Documents/Code/Personal/Rust/micro-apps/account-service/target/debug/deps/account_service-abce2ba0f2188f03.d: src/main.rs
-
-src/main.rs:
--- account-service/target/debug/deps/account_service-ac1d0b0bcc10a248.d
+++
@@ -1,5 +0,0 @@
-/Users/dainguyen.iammm/Documents/Code/Personal/Rust/micro-apps/account-service/target/debug/deps/libaccount_service-ac1d0b0bcc10a248.rmeta: src/main.rs
-
-/Users/dainguyen.iammm/Documents/Code/Personal/Rust/micro-apps/account-service/target/debug/deps/account_service-ac1d0b0bcc10a248.d: src/main.rs
-
-src/main.rs:
